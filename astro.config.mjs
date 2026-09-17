34b5393174340fd61b3fbcf74adb6aaa31cbc953
Flavio Ore <101116042+Flavio-Ore@users.noreply.github.com>
2026-09-17 11:15:56 -0500

    feat: Imgs .avif .webp added feat: github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,7 @@ import tailwind from '@astrojs/tailwind'
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://Flavio-Ore.github.io',
+  base: '/Gonfolio'
 })
